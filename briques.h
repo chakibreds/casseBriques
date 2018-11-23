23cc2c34da2e45837ddc77e9ea8e1313c07a5fc1
LeifHenriksen <44870274+LeifHenriksen@users.noreply.github.com>
2018-11-23 11:57:57 +0100

    Add files via upload

--- briques.h
+++ briques.h
@@ -3,24 +3,25 @@
 #include"window.h"
 class Brique
 {
-private: 
-char corp; 
-unsigned int resistance,L,l,x,y;
-public:
-	//largeur L=hauteur 
-	//longueur l 
-	Brique(); 
-	Brique(char corp ,int y, int x , int L, int l,int resistance);
-	Brique(const Brique &b);
-	unsigned int getL()const; 
-	unsigned int getl()const;
-	unsigned int getX()const;
-	unsigned int getY()const;
-	void setL(unsigned int L);
-	void setl(unsigned int l);
-	void setX(unsigned int x);
-	void setY(unsigned int y);
-	void printBrique(WINDOW *w);
-	void printvide(WINDOW *w);
+ private: 
+  char corp; 
+  unsigned int resistance,L,l,x,y;
+  Color couleur;
+ public:
+  //largeur L=hauteur 
+  //longueur l 
+  Brique(); 
+  Brique(char corp , Color couleur,int y, int x , int L, int l,int resistance);
+  Brique(const Brique &b);
+  unsigned int getL()const; 
+  unsigned int getl()const;
+  unsigned int getX()const;
+  unsigned int getY()const;
+  void setL(unsigned int L);
+  void setl(unsigned int l);
+  void setX(unsigned int x);
+  void setY(unsigned int y);
+  void printBrique(WINDOW *w);
+  void printVide(WINDOW *w);
 };
 #endif
